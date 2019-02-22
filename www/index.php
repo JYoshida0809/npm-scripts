4f68df88da65504c853a45ad589b02deab1c9a22
J.Yoshida <yoshida@n-e-u.co.jp>
2019-02-22 18:43:38 +0900

    upd base

--- www/index.php
+++ www/index.php
@@ -17,32 +17,17 @@ $bodyClass = '';
 <main>
 <article>
 
-  <section class="RestaurantCafe">
-    <div class="RestaurantCafe__background">
-      <h2 class="@Title -reversal">
-        <span class="@Title__english"><span>RESTAURANT &amp; CAFE</span></span>
-        <span class="@Title__japanese"><span>レストラン&amp;カフェ</span></span>
-      </h2>
-      <div class="RestaurantCafe__thumbnailWrap">
-        <div class="RestaurantCafe__thumbnail">
-          <div class="RestaurantCafe__thumbnailItem"><img src="https://placehold.jp/250x250.png" alt="" class="RestaurantCafe__thumbnailImage"></div>
-          <div class="RestaurantCafe__thumbnailItem"><img src="https://placehold.jp/250x250.png" alt="" class="RestaurantCafe__thumbnailImage"></div>
-          <div class="RestaurantCafe__thumbnailItem"><img src="https://placehold.jp/250x250.png" alt="" class="RestaurantCafe__thumbnailImage"></div>
-          <div class="RestaurantCafe__thumbnailItem"><img src="https://placehold.jp/250x250.png" alt="" class="RestaurantCafe__thumbnailImage"></div>
-          <div class="RestaurantCafe__thumbnailItem"><img src="https://placehold.jp/250x250.png" alt="" class="RestaurantCafe__thumbnailImage"></div>
-          <div class="RestaurantCafe__thumbnailItem"><img src="https://placehold.jp/250x250.png" alt="" class="RestaurantCafe__thumbnailImage"></div>
-          <div class="RestaurantCafe__thumbnailItem"><img src="https://placehold.jp/250x250.png" alt="" class="RestaurantCafe__thumbnailImage"></div>
-          <div class="RestaurantCafe__thumbnailItem"><img src="https://placehold.jp/250x250.png" alt="" class="RestaurantCafe__thumbnailImage"></div>
-          <div class="RestaurantCafe__thumbnailItem"><img src="https://placehold.jp/250x250.png" alt="" class="RestaurantCafe__thumbnailImage"></div>
-          <div class="RestaurantCafe__thumbnailItem"><img src="https://placehold.jp/250x250.png" alt="" class="RestaurantCafe__thumbnailImage"></div>
-        </div>
-      </div>
-      <p class="RestaurantCafe__text">和食・中華・イタリアンからスイーツまで。<br>多数のメニューバリエーションからお選びいただけます。多数のメニューバリエーションからお選びいただけます。</p>
-      <div class="RestaurantCafe__button"><a href="sample" class="@ButtonB -reversal"><span class="@ButtonB__adjust">一覧をみる</span></a></div>
-    </div>
-  </section><!-- .RestaurantCafe -->
 
-  <p class="@TestTitle">テストタイトルです</p>
+  <section class="MainContetns">
+    <div class="MainContents__inner">
+      <h2 class="@Title">
+        <span class="@Title__english"><span>english</span></span>
+        <span class="@Title__japanese"><span>日本語</span></span>
+      </h2>
+
+      <p><a href="sample" class="@ButtonA">ButtonA</a></p>
+    </div>
+  </section>
 
 </article>
 </main>
